169d0a60a581256e7656f000e26b812e5c9cefa8
Juan Amari <juanamari@Juans-MacBook-Pro.local>
2016-09-04 13:20:53 -0300

    Removed some redundant documentation.

--- State/src/patterns/state/IllegalOperationException.java
+++ State/src/patterns/state/IllegalOperationException.java
@@ -1,8 +1,5 @@
 package patterns.state;
 
-/**
- * Created by juanamari on 9/4/16.
- */
 public class IllegalOperationException extends Exception {
     public IllegalOperationException(String message){
         super(message);
--- State/src/patterns/state/NotEnoughFundsException.java
+++ State/src/patterns/state/NotEnoughFundsException.java
@@ -1,8 +1,5 @@
 package patterns.state;
 
-/**
- * Created by juanamari on 9/4/16.
- */
 public class NotEnoughFundsException extends Exception{
     public NotEnoughFundsException(String message){
         super(message);
